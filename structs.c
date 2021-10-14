be3045264f78fca970280931a42d31d7b9a6f683
Sarah Leong <sleong20@stuy.edu>
2021-10-14 13:03:03 -0400

    created the file

---
+++ structs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "structs.h"
+
+struct cat {char[] name; int age;};
+
+// function to print out the values in the struct
+// function to make a new struct
+// main function to test
