bdc20542cc6d92170dd86b25f803a1718910d646
Sarah Leong <sleong20@stuy.edu>
2021-10-14 15:34:44 -0400

    fixed print_cat to take pointer of struct cat; add_cat works

--- structs.c
+++ structs.c
@@ -3,30 +3,29 @@
 struct cat {char name[20]; int age; char breed[20];};
 
 // function to print out the values in the struct
-void print_cat(struct cat c) {
+void print_cat(struct cat *c) {
     printf("This is the information on the cat, %s. %s is %d years old and is a %s.\n",
-        c.name,c.name,c.age,c.breed);
+        c->name,c->name,c->age,c->breed);
 }
 
 // function to make a new struct
-struct cat * add_cat(char n[20], int a, b[20]) {
+struct cat * add_cat(char n[20], int a, char b[20]) {
     struct cat * c = malloc(sizeof(struct cat));
-    c -> name = n;
+    strcpy(c->name, n);
+    strcpy(c->breed, b);
     c -> age = a;
-    c -> breed = b;
     return c;
 }
 
 // main function
 int main() {
-    struct cat Joline;
-    strcpy(Joline.name, "Joline");
-    Joline.age = 5;
-    strcpy(Joline.breed, "Calico");
+    struct cat * Joline;
+    strcpy(Joline->name, "Joline");
+    Joline->age = 5;
+    strcpy(Joline->breed, "Calico");
     print_cat(Joline);
 
-    struct cat * Mark;
-    Mark = add_cat("Mark", 3, "Siamese");
+    struct cat * Mark = add_cat("Mark", 3, "Siamese");
     print_cat(Mark);
 
     return 0;
